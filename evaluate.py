1fd06798dda2901e6ed91055932826d0d257c7fd
Lê Viết Mẫn <manleviet@gmail.com>
2022-08-07 23:09:43 +0200

    Evaluation results of fastdiagp_v4_1.py - breadth first search

--- evaluate.py
+++ evaluate.py
@@ -5,7 +5,7 @@ import time
 
 # logging.config.fileConfig('logging.conf')
 
-cards = ["8", "16"]  # ["1", "2", "4"]
+cards = ["1", "2", "4", "8", "16"]  # []
 # lmaxs = ["0"]  # , "1", "2", "3", "4", "5"]
 cores = ["2", "4", "6", "8"]
 numScenarios = 10
@@ -19,20 +19,20 @@ for card in cards:
         reqPath = "./data/linux/prod_{}_{}.cnf".format(card, i + 1)
         # for lmax in lmaxs:
         #     if lmax == "0":
-        print(
-            "python3 ./eval_fastdiag.py " + modelPath + " " + reqPath + " " + solver_path)
-        os.system(
-            "python3 ./eval_fastdiag.py " + modelPath + " " + reqPath + " " + solver_path + " >>" + " resultFastDiag.csv")
-            # else:
-            #     print(
-            #         "python3 ./eval_fastdiagp_v1.py " + modelPath + " " + reqPath + " " + solver_path + " " + lmax)
-            #     os.system(
-            #         "python3 ./eval_fastdiagp_v1.py " + modelPath + " " + reqPath + " " + solver_path + " " + lmax + " >>" + " resultFastDiagPV1.csv")
-            #
-            #     print(
-            #         "python3 ./eval_fastdiagp_v2.py " + modelPath + " " + reqPath + " " + solver_path + " " + lmax)
-            #     os.system(
-            #         "python3 ./eval_fastdiagp_v2.py " + modelPath + " " + reqPath + " " + solver_path + " " + lmax + " >>" + " resultFastDiagPV2.csv")
+        # print(
+        #     "python3 ./eval_fastdiag.py " + modelPath + " " + reqPath + " " + solver_path)
+        # os.system(
+        #     "python3 ./eval_fastdiag.py " + modelPath + " " + reqPath + " " + solver_path + " >>" + " resultFastDiag.csv")
+        # else:
+        #     print(
+        #         "python3 ./eval_fastdiagp_v1.py " + modelPath + " " + reqPath + " " + solver_path + " " + lmax)
+        #     os.system(
+        #         "python3 ./eval_fastdiagp_v1.py " + modelPath + " " + reqPath + " " + solver_path + " " + lmax + " >>" + " resultFastDiagPV1.csv")
+        #
+        #     print(
+        #         "python3 ./eval_fastdiagp_v2.py " + modelPath + " " + reqPath + " " + solver_path + " " + lmax)
+        #     os.system(
+        #         "python3 ./eval_fastdiagp_v2.py " + modelPath + " " + reqPath + " " + solver_path + " " + lmax + " >>" + " resultFastDiagPV2.csv")
             #
             #     print(
             #         "python3 ./eval_fastdiagp_v3.py " + modelPath + " " + reqPath + " " + solver_path + " " + lmax)
@@ -45,15 +45,20 @@ for card in cards:
             # os.system(
             #     "python3 ./eval_fastdiagp_v1_1.py " + modelPath + " " + reqPath + " " + solver_path + " " + core + " >>" + " resultFastDiagPV1_1.csv")
 
-            print(
-                "python3 ./eval_fastdiagp_v2_1.py " + modelPath + " " + reqPath + " " + solver_path + " " + core)
-            os.system(
-                "python3 ./eval_fastdiagp_v2_1.py " + modelPath + " " + reqPath + " " + solver_path + " " + core + " >>" + " resultFastDiagPV2_1.csv")
+            # print(
+            #     "python3 ./eval_fastdiagp_v2_1.py " + modelPath + " " + reqPath + " " + solver_path + " " + core)
+            # os.system(
+            #     "python3 ./eval_fastdiagp_v2_1.py " + modelPath + " " + reqPath + " " + solver_path + " " + core + " >>" + " resultFastDiagPV2_1.csv")
 
             # print(
             #     "python3 ./eval_fastdiagp_v3_1.py " + modelPath + " " + reqPath + " " + solver_path + " " + core)
             # os.system(
             #     "python3 ./eval_fastdiagp_v3_1.py " + modelPath + " " + reqPath + " " + solver_path + " " + core + " >>" + " resultFastDiagPV3_1.csv")
+
+            print(
+                "python3 ./eval_fastdiagp_v4_1.py " + modelPath + " " + reqPath + " " + solver_path + " " + core)
+            os.system(
+                "python3 ./eval_fastdiagp_v4_1.py " + modelPath + " " + reqPath + " " + solver_path + " " + core + " >>" + " resultFastDiagPV4_1.csv")
 
 total_time = time.time() - start_time
 print("Sat4j time: " + str(total_time))
