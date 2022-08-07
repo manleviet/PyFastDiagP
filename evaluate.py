6a4a6bf121cec794577e008143f3ab51df311a97
Lê Viết Mẫn <manleviet@gmail.com>
2022-08-07 18:22:34 +0200

    Update evaluate.py
    Evaluate card=8,16 for fastdiagp_v2_1

--- evaluate.py
+++ evaluate.py
@@ -5,8 +5,8 @@ import time
 
 # logging.config.fileConfig('logging.conf')
 
-cards = ["1", "2", "4"]  # , "8", "16"]
-lmaxs = ["0", "1", "2", "3", "4", "5"]
+cards = ["8", "16"]  # ["1", "2", "4"]
+# lmaxs = ["0"]  # , "1", "2", "3", "4", "5"]
 cores = ["2", "4", "6", "8"]
 numScenarios = 10
 
@@ -17,43 +17,43 @@ modelPath = "./data/linux/linux.cnf"
 for card in cards:
     for i in range(numScenarios):
         reqPath = "./data/linux/prod_{}_{}.cnf".format(card, i + 1)
-        for lmax in lmaxs:
-            if lmax == "0":
-                print(
-                    "python3 ./eval_fastdiag.py " + modelPath + " " + reqPath + " " + solver_path)
-                os.system(
-                    "python3 ./eval_fastdiag.py " + modelPath + " " + reqPath + " " + solver_path + " >>" + " resultFastDiag.csv")
-            else:
-                print(
-                    "python3 ./eval_fastdiagp_v1.py " + modelPath + " " + reqPath + " " + solver_path + " " + lmax)
-                os.system(
-                    "python3 ./eval_fastdiagp_v1.py " + modelPath + " " + reqPath + " " + solver_path + " " + lmax + " >>" + " resultFastDiagPV1.csv")
-
-                print(
-                    "python3 ./eval_fastdiagp_v2.py " + modelPath + " " + reqPath + " " + solver_path + " " + lmax)
-                os.system(
-                    "python3 ./eval_fastdiagp_v2.py " + modelPath + " " + reqPath + " " + solver_path + " " + lmax + " >>" + " resultFastDiagPV2.csv")
-
-                print(
-                    "python3 ./eval_fastdiagp_v3.py " + modelPath + " " + reqPath + " " + solver_path + " " + lmax)
-                os.system(
-                    "python3 ./eval_fastdiagp_v3.py " + modelPath + " " + reqPath + " " + solver_path + " " + lmax + " >>" + " resultFastDiagPV3.csv")
+        # for lmax in lmaxs:
+        #     if lmax == "0":
+        print(
+            "python3 ./eval_fastdiag.py " + modelPath + " " + reqPath + " " + solver_path)
+        os.system(
+            "python3 ./eval_fastdiag.py " + modelPath + " " + reqPath + " " + solver_path + " >>" + " resultFastDiag.csv")
+            # else:
+            #     print(
+            #         "python3 ./eval_fastdiagp_v1.py " + modelPath + " " + reqPath + " " + solver_path + " " + lmax)
+            #     os.system(
+            #         "python3 ./eval_fastdiagp_v1.py " + modelPath + " " + reqPath + " " + solver_path + " " + lmax + " >>" + " resultFastDiagPV1.csv")
+            #
+            #     print(
+            #         "python3 ./eval_fastdiagp_v2.py " + modelPath + " " + reqPath + " " + solver_path + " " + lmax)
+            #     os.system(
+            #         "python3 ./eval_fastdiagp_v2.py " + modelPath + " " + reqPath + " " + solver_path + " " + lmax + " >>" + " resultFastDiagPV2.csv")
+            #
+            #     print(
+            #         "python3 ./eval_fastdiagp_v3.py " + modelPath + " " + reqPath + " " + solver_path + " " + lmax)
+            #     os.system(
+            #         "python3 ./eval_fastdiagp_v3.py " + modelPath + " " + reqPath + " " + solver_path + " " + lmax + " >>" + " resultFastDiagPV3.csv")
 
         for core in cores:
-            print(
-                "python3 ./eval_fastdiagp_v1_1.py " + modelPath + " " + reqPath + " " + solver_path + " " + core)
-            os.system(
-                "python3 ./eval_fastdiagp_v1_1.py " + modelPath + " " + reqPath + " " + solver_path + " " + core + " >>" + " resultFastDiagPV1_1.csv")
+            # print(
+            #     "python3 ./eval_fastdiagp_v1_1.py " + modelPath + " " + reqPath + " " + solver_path + " " + core)
+            # os.system(
+            #     "python3 ./eval_fastdiagp_v1_1.py " + modelPath + " " + reqPath + " " + solver_path + " " + core + " >>" + " resultFastDiagPV1_1.csv")
 
             print(
                 "python3 ./eval_fastdiagp_v2_1.py " + modelPath + " " + reqPath + " " + solver_path + " " + core)
             os.system(
                 "python3 ./eval_fastdiagp_v2_1.py " + modelPath + " " + reqPath + " " + solver_path + " " + core + " >>" + " resultFastDiagPV2_1.csv")
 
-            print(
-                "python3 ./eval_fastdiagp_v3_1.py " + modelPath + " " + reqPath + " " + solver_path + " " + core)
-            os.system(
-                "python3 ./eval_fastdiagp_v3_1.py " + modelPath + " " + reqPath + " " + solver_path + " " + core + " >>" + " resultFastDiagPV3_1.csv")
+            # print(
+            #     "python3 ./eval_fastdiagp_v3_1.py " + modelPath + " " + reqPath + " " + solver_path + " " + core)
+            # os.system(
+            #     "python3 ./eval_fastdiagp_v3_1.py " + modelPath + " " + reqPath + " " + solver_path + " " + core + " >>" + " resultFastDiagPV3_1.csv")
 
 total_time = time.time() - start_time
 print("Sat4j time: " + str(total_time))
